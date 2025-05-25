4d9b6ea5b57b4824b749c6a8721845e8a26a8aee
Nithya-89-git <nithyaanantharaj@gmail.com>
2025-05-25 12:32:05 +0530

    project code added

---
+++ src/org/phone/PhoneInfo.java
@@ -0,0 +1,42 @@
+package org.phone;
+
+public class PhoneInfo {
+	private void phoneName() {
+		System.out.println("Lenova");
+		// TODO Auto-generated method stub
+
+	}
+	private void phoneImeinum() {
+		System.out.println("123456789012345");
+		// TODO Auto-generated method stub
+
+	}
+	private void camera() {
+		System.out.println("FHD");
+		// TODO Auto-generated method stub
+
+	}
+	private void storage() {
+		System.out.println("100MB");
+		// TODO Auto-generated method stub
+
+	}
+	private void osName() {
+		System.out.println("Android");
+		// TODO Auto-generated method stub
+
+	}
+	public static void main(String[] args) {
+		PhoneInfo d = new PhoneInfo();
+		d.phoneName();
+		d.phoneImeinum();
+		d.camera();
+		d.storage();
+		d.osName();
+	}
+		// TODO Auto-generated method stub
+
+	}
+
+
+
